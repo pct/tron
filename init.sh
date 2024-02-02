29d06854d893bdc24e191b7c850a78419c1720ed
Jin-Sih Lin <linpct@gmail.com>
2024-02-03 00:37:18 +0800

    init

---
+++ init.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# 仿 rails 結構，建出一個簡單的 MVC 架構
+
+ mkdir -p app
+ mkdir -p app/controllers
+ mkdir -p app/models
+ mkdir -p app/views
+ mkdir -p config
+ mkdir -p db
+ mkdir -p lib
+ mkdir -p public
+ mkdir -p test
+ mkdir -p tmp
+ mkdir -p vendor
+ touch app/__init__.py
+ touch app/controllers/__init__.py
+ touch app/models/__init__.py
+ touch app/views/__init__.py
+ touch config/__init__.py
+ touch db/__init__.py
+ touch lib/__init__.py
+ touch public/__init__.py
+ touch test/__init__.py
+ touch tmp/__init__.py
+ touch vendor/__init__.py
+ touch app/controllers/home_controller.py
+ touch app/models/user.py
+ touch app/views/home
+ touch config/routes.py
+ touch config/settings.py
+ touch public/index.html
+ touch test/test.py
+ touch tmp/tmp.txt
+ touch vendor/vendor.py
